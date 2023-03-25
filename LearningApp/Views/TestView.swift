41397ab9aff0137f20e7b7aedc0069fcc4981583
Alextran0899 <Alextran0899@gmail.com>
2023-03-24 19:09:18 -0500

    - add submit button in test view - highlight appropriate color in test view button

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -9,29 +9,82 @@ import SwiftUI
 
 struct TestView: View {
     @EnvironmentObject var model: ContentModel
+    @State private var selectedAnswerIndex: Int?
+    @State var numCorrect = 0
+    @State var submitted = false
+    
     var body: some View {
         if model.currentQuestion != nil {
-            VStack{
+            VStack(alignment: .leading){
                 // Question number
                 Text("Question \(model.currentQuestionIndex! + 1) of \(model.currModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 // Question
                 CodeTextView()
+                    .padding(.horizontal, 20)
                 // Answers
-                // Button
-            }.navigationBarTitle("\(model.currModule?.category ?? "") Test")
+                ScrollView {
+                    VStack (alignment: .leading){
+                        ForEach(0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            Button {
+                                // Track the selected index
+                                selectedAnswerIndex = index
+                            } label: {
+                                ZStack{
+                                    if submitted == false {
+                                        RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                            .frame(height: 48)
+                                    } else {
+                                        if (index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex) || index == model.currentQuestion!.correctIndex {
+                                            RectangleCard(color: .green)
+                                                .frame(height: 48)
+                                        } else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex {
+                                            RectangleCard(color: .red)
+                                                .frame(height: 48)
+                                        } else {
+                                            RectangleCard()
+                                                .frame(height: 48)
+                                        }
+                                    }
+                                    Text(model.currentQuestion!.answers[index])
+                                        .foregroundColor(.black)
+                                        .bold()
+                                }
+                            }.disabled(submitted)
+                        }
+                    }.padding(20)
+                }
+                Button {
+                    submitted = true
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        submitted = true
+                    }
+                } label: {
+                    ZStack{
+                        RectangleCard(color: .green)
+                            .frame(height: 48)
+                        Text("Submit")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }.disabled(selectedAnswerIndex == nil)
+                    .padding(.horizontal, 20)
+            }
+            .navigationBarTitle("\(model.currModule?.category ?? "") Test")
         } else {
             ProgressView()
         }
     }
 }
-
-struct TestView_Previews: PreviewProvider {
-    static var previews: some View {
-        let model = ContentModel()
-        TestView()
-            .onAppear{
-                model.beginTest(0)
-            }
-            .environmentObject(model)
-    }
-}
+//
+//struct TestView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        let model = ContentModel()
+//        TestView()
+//            .onAppear {
+//                model.beginModule(0)
+//                model.beginTest(0)
+//            }
+//            .environmentObject(model)
+//    }
+//}
